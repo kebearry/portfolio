1b66e09d49971ce6b29c3e4dbfeb14bdc040b879
Tan, Rebecca Li Kiat <rebecca.li.kiat.tan@accenture.com>
2020-03-01 14:19:35 +0800

    website

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ module.exports = {
         meta: [
             { charset: 'utf-8' },
             { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-            { hid: 'description', name: 'description', content: 'Portfolio Site for Rebecca, ApplicationDeveloper' },
+            { hid: 'description', name: 'description', content: 'Portfolio Site for Rebecca, Application Developer' },
         ],
         link: [
             { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
